0b4f3b582f69851edb8a05af0f736d86fb85a426
Ernest Micklei <ernest.micklei@gmail.com>
2021-02-18 21:40:10 +0100

    test sound

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"time"
 
 	"gitlab.com/gomidi/rtmididrv/imported/rtmidi"
 	// when using portmidi, replace the line above with
 	// driver gitlab.com/gomidi/portmididrv
 )
 
+var port = flag.Int("p", -1, "port number for out")
+
 func main() {
+	flag.Parse()
+
 	in, err := rtmidi.NewMIDIInDefault()
 	if err != nil {
 		log.Fatalln("can't open default MIDI in: ", err)
@@ -24,7 +30,7 @@ func main() {
 		if err != nil {
 			name = ""
 		}
-		fmt.Println(name)
+		fmt.Println(i, name)
 	}
 	{
 		// Outs
@@ -43,7 +49,25 @@ func main() {
 			if err != nil {
 				name = ""
 			}
-			fmt.Println(name)
+			fmt.Println(i, name)
 		}
 	}
+	if *port == -1 {
+		return
+	}
+
+	out, err := rtmidi.NewMIDIOutDefault()
+	if err != nil {
+		log.Fatalln("can't open default MIDI out: ", err)
+	}
+	defer out.Close()
+
+	err = out.OpenPort(*port, "")
+	if err != nil {
+		log.Fatalln("can't open default MIDI out: ", err)
+	}
+
+	out.SendMessage([]byte{0x90, 60, 60})
+	time.Sleep(1 * time.Second)
+	out.SendMessage([]byte{0x80, 60, 60})
 }
